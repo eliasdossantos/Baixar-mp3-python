075177d303cbced2104a1a665f4aa6f13c5e948e
Elias Antonio <105184013+eliasdossantos@users.noreply.github.com>
2022-11-18 13:30:49 -0300

    Add files via upload

---
+++ baixar-mp3.py
@@ -0,0 +1,32 @@
+
+from pytube import YouTube
+import moviepy.editor as mp
+import re
+import os
+
+# Instalação das biblioteca do python pelo cmd ou o PowerShell com os comandos abaixo
+# pip install pytube
+# pip install moviepy
+# pip shell
+
+# Digite o link do video e a pasta que deseja salvar o mp3:
+link = input("Digite o link do video que deseja baixar: ")
+path = input("Digite o diretório que deseja salvar o video: ")
+yt = YouTube(link)
+
+# Vai começar o download
+print("Titulo = ", yt.title)
+print("Baixando... ")
+ys = yt.streams.filter(only_audio=True).first().download(path)
+print("Download completo! ")
+
+# Converte o mp4 para mp3
+print('Convertendo arquivo... ')
+for file in os.listdir(path):
+    if re.search('mp4', file):
+        mp4_path = os.path.join(path, file)
+        mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
+        new_file = mp.AudioFileClip(mp4_path)
+        new_file.write_audiofile(mp3_path)
+        os.remove(mp4_path)
+print('Sucesso!')
